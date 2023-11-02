6e1deace6a6d1038d3c41f74ff87bd3a59e96d54
EddV23 <edjevan40@gmail.com>
2023-11-02 18:29:40 +0000

    imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-"""
-imports the function def add(a, b): from the file add_0.py
-and prints the result of the addition 1 + 2 = 3
-"""
 from add_0 import add
-
 if __name__ == "__main__":
     a = 1
     b = 2
