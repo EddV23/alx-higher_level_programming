a5ac5a59d91215e38585ffb73f0cc6818cdef31d
EddV23 <edjevan40@gmail.com>
2023-11-08 20:27:48 +0000

    print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -14,8 +14,8 @@ void print_python_list(PyObject *p)
 	PyListObject *alloc;
 	PyObject *obj;
 
-	size = PyList_Size(p);
-	/*size = ((PyVarObject *)(p))->ob_size;*/
+	/*size = PyList_Size(p);*/
+	size = ((PyVarObject *)(p))->ob_size;
 	alloc = (PyListObject *)p;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
@@ -23,7 +23,9 @@ void print_python_list(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
+		/*obj = PyList_GetItem(p, i);*/
 		obj = ((PyListObject *)p)->ob_item[i];
+		/*printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);*/
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
@@ -47,8 +49,10 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+	/*size = PyBytes_Size(p);*/
+	size = ((PyVarObject *)(p))->ob_size;
+	/*str = PyBytes_AsString(p);*/
+	str = ((PyBytesObject *)p)->ob_sval;
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", str);
 	/*printf("  first %ld bytes:", size < 10 ? size + 1 : 10);*/
