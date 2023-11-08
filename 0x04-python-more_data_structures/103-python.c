44533061d55e19b0674fd8c27dc2526493503a99
EddV23 <edjevan40@gmail.com>
2023-11-08 19:37:33 +0000

    print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,25 +1,32 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_bytes(PyObject *p);
+
 /**
  * print_python_list - prints some basic info about Python lists
  * @p: PyObject list
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, alloc, i;
+	Py_ssize_t size, i;
+	/*long int size, i;*/
+	PyListObject *alloc;
 	PyObject *obj;
 
 	size = PyList_Size(p);
-	alloc = ((PyListObject *)p)->allocated;
+	/*size = ((PyVarObject *)(p))->ob_size;*/
+	alloc = ((PyListObject *)p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", alloc);
+	printf("[*] Allocated = %ld\n", alloc->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		obj = PyList_GetItem(p, i);
 		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
 	}
 }
 
@@ -29,7 +36,8 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
+	Py_ssize_t size, i, delim;
+	/*long int size, i, delim;*/
 	char *str;
 
 	printf("[.] bytes object info\n");
@@ -38,12 +46,24 @@ void print_python_bytes(PyObject *p)
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
 	size = PyBytes_Size(p);
 	str = PyBytes_AsString(p);
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", str);
-	printf("  first %ld bytes:", size < 10 ? size + 1 : 10);
-	for (i = 0; i < size && i < 10; i++)
-		printf(" %02hhx", str[i]);
+	/*printf("  first %ld bytes:", size < 10 ? size + 1 : 10);*/
+	if (size < 10)
+		delim = size + 1;
+	else
+		delim = 10;
+	printf(" first %ld bytes:", delim);
+
+	for (i = 0; i < delim; i++)
+	{
+		if (str[i] < 0)
+			printf(" %02x", 256 + str[i]);
+		else
+			printf(" %02x", str[i]);
+	}
 	printf("\n");
 }
