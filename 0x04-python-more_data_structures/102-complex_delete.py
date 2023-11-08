d7db291203a8e13e0860077176accdf2f0f8e27d
EddV23 <edjevan40@gmail.com>
2023-11-08 11:37:46 +0000

    deletes keys with a specific value in a dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+deletes keys with a specific value in a dictionary
+"""
+
+
+def complex_delete(a_dictionary, value):
+    """
+    [a_dictionary.pop(key) for key, val in list(a_dictionary.items()) if val == value]
+    return a_dictionary
+    """
+    for key, val in list(a_dictionary.items()):
+        if val == value:
+            a_dictionary.pop(key)
+    return a_dictionary
