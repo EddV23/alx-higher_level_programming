82d2e3ccc1b8dc05f7c761ef94f507cb37da86a2
EddV23 <edjevan40@gmail.com>
2023-11-08 14:40:06 +0000

    deletes keys with a specific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,7 +6,8 @@ deletes keys with a specific value in a dictionary
 
 def complex_delete(a_dictionary, value):
     """
-    [a_dictionary.pop(key) for key, val in list(a_dictionary.items()) if val == value]
+    [a_dictionary.pop(key) for key, val in list(a_dictionary.items())
+if val == value]
     return a_dictionary
     """
     for key, val in list(a_dictionary.items()):
