df04f1c5a812f3d640dff19a50446bbbc19720f3
EddV23 <edjevan40@gmail.com>
2023-11-21 04:23:02 +0000

    used for disassembly of given bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,19 +2,21 @@
 """
 does exactly the same as a specific
 Python bytecode
+used for disassembly of given bytecode
 """
 
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-
-            resutlt += (a ** b) / i
+            else:
+                resutlt += (a ** b) / i
 
         except Exception:
             result += b + a
+            break
 
     return result
