0c8e0f60ce52319e515c3d7d37e52d216250f750
EddV23 <edjevan40@gmail.com>
2023-11-28 22:01:47 +0000

    0x07. Python - Test-Driven Development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,7 +4,6 @@ Module for lazy matrix multiplication using NumPy.
 
 
 """
-
 import numpy as np
 
 
@@ -12,13 +11,6 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using NumPy
     """
-    if not m_a or not all(row for row in m_a) or not m_b or not all(
-            row for row in m_b):
-        raise ValueError("m_a can't be empty or m_b can't be empty")
 
-    try:
-        result = np.dot(np.array(m_a), np.array(m_b))
-    except ValueError as e:
-        raise ValueError("m_a and m_b can't be multiplied") from e
-
+    result = np.matmul(m_a, m_b)
     return result
