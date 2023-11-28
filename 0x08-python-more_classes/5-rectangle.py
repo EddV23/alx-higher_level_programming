a49709e84d86d12254976569c8f8116b9c4d49f0
EddV23 <edjevan40@gmail.com>
2023-11-28 01:37:58 +0000

    a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initializes an instance of the Rectangle class"""
-        self.__width = width
+        self.width = width
         self.height = height
 
     @property
