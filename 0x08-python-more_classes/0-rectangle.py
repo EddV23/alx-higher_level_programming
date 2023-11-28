ead815f5a1c5c2ab34469383871f3c71a2d61124
EddV23 <edjevan40@gmail.com>
2023-11-28 00:00:47 +0000

    an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module that creates an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Represents a Rectangle class used to create rectangle objects"""
+    pass
