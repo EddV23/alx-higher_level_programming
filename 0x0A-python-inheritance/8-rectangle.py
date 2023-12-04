c785aec1c6c84f29fb56019e7278e515c2f4249a
EddV23 <edjevan40@gmail.com>
2023-12-04 20:42:20 +0000

    defines a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,6 +11,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Instantiates with width and height, validates them."""
+        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
