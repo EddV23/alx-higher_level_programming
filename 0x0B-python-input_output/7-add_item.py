ed7eb8fd8c07d39a202a28fab58cf346db5bd1ab
EddV23 <edjevan40@gmail.com>
2023-12-05 13:12:40 +0000

    defines a script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that defines a script that adds all arguments
+to a Python list, and then save them to a file
+"""
+import sys
+
+
+if __name__ == "__main__":
+    """Parse command line arguments"""
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    """Load existing list from file or create an empty list"""
+    try:
+        my_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
