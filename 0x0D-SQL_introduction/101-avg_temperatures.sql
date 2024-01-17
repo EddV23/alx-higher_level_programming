ff89f6a9fc8957316c1cf4018b92ef5856d4a56f
EddV23 <edjevan40@gmail.com>
2024-01-17 05:06:35 +0000

    mysql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,2 @@
 -- Display the average temperature by city ordered by temperature (descending)
-USE hbtn_0c_0;
 SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
