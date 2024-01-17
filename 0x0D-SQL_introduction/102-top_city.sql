c0d9ef44a94f5912ed80b0e60b5086247a9692a4
EddV23 <edjevan40@gmail.com>
2024-01-17 05:25:21 +0000

    mysql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Display the top 3 cities' temperature during July and August ordered by tempe
 -- rature (descending)
-SELECT city, AVG(value) AS avg_temp
+SELECT city, AVG(`value`) AS avg_temp
 FROM temperatures
 WHERE month IN ('July', 'August')
 GROUP BY city
