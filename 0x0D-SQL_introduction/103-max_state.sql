8f248b05b54ef7adedabe265e67f54789053d9e4
EddV23 <edjevan40@gmail.com>
2024-01-17 05:18:12 +0000

    mysql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,5 @@
 -- Display the max temperature of each state ordered by state name
-USE hbtn_0c_0;
-SELECT state, MAX(temperature) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(temperature) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
