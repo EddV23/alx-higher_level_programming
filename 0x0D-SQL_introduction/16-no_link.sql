2adc7f795c1f389e1c2bd5bfac8b652fd2c2916a
EddV23 <edjevan40@gmail.com>
2024-01-17 05:21:20 +0000

    mysql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- List all records of the second_table without rows with empty name
-SELECT * FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT `score`, `name`
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
