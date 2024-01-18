dfb22b8c2f39a4e806b78a12f9644c907a21e983
EddV23 <edjevan40@gmail.com>
2024-01-18 02:19:46 +0000

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,12 @@
--- creates the table unique_id on your MySQL server
+-- Step 1: Create the table with a unique constraint
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-);
+    id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(256) UNIQUE
+	);
+
+-- Step 2: Insert the data into the new table, which will only keep unique entries
+INSERT IGNORE INTO unique_id (name)
+SELECT name FROM unique_id;
+
+-- Step 3: Drop the old table
+DROP TABLE unique_id;
