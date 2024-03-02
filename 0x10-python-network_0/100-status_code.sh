bf21e6fd853673e258e05458cd287a604addab29
EddV23 <edjevan40@gmail.com>
2024-03-02 02:31:05 +0000

    0x10. Python - Network #0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument,
-# and displays only the status code of the response.
-# not allowed to use any pipe, redirection, etc.
-# not allowed to use ; and &&
+# sends request to URL passed as argument, displays only status code of response.
 curl -s -o /dev/null -w "%{http_code}" "$1"
